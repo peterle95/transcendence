61301efb8c7c98007879cc08169690e9af1ae57e
Antoine <antoine.hugi@gmail.com>
2026-03-04 11:06:26 +0100

    cleaned up env variables

--- auth_srvc/next.config.js
+++ auth_srvc/next.config.js
@@ -14,11 +14,6 @@ const nextConfig = {
 		]
 	},
 	reactStrictMode: true,
-	env: {
-		AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || 'http://localhost:3000',
-		CHAT_SERVICE_URL: process.env.CHAT_SERVICE_URL || 'http://localhost:3001',
-	},
-
 };
 
 module.exports = nextConfig;
