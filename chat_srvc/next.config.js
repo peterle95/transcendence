0061c4cfc28cef9866f67497fc7e3a7720696b41
Peter Moelzer <moelzerpeter@gmail.com>
2026-03-25 16:06:49 +0100

    correct basePath in next.config.js files

--- chat_srvc/next.config.js
+++ chat_srvc/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/chat',
   env: {
     NEXT_PUBLIC_AUTH_SERVICE_URL: process.env.NEXT_PUBLIC_AUTH_SERVICE_URL || 'http://localhost:3000',
   },
