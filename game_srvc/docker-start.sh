e47b54ecea6cff1c9859bc83e7cec1bdf57d9545
Peter Mölzer <moelzerpeter@gmail.com>
2026-04-24 13:26:52 +0000

    websocket and webpack fix

--- game_srvc/docker-start.sh
+++ game_srvc/docker-start.sh
@@ -3,7 +3,7 @@ set -e
 
 #script consider value of NODE_ENV and runs accordingly
 if [ "$NODE_ENV" = "production" ]; then
-npx prisma migrate deploy
+npx prisma db push
 npm run start:socket &
 SOCKET_PID=$!
 trap 'kill -TERM "$SOCKET_PID" 2>/dev/null || true' INT TERM
